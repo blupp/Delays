d5852f5c338ee57b5edc277fae5bc510b617c306
Sebastian Björkelid <sebastian.bjorkelid@gmail.com>
2014-09-10 12:24:33 +0200

    Added chart lib EazeGraph

--- app/src/main/java/com/bjorkelid/delays/StatsActivity.java
+++ app/src/main/java/com/bjorkelid/delays/StatsActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.parse.FindCallback;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.GetCallback;
 import com.parse.ParseException;
+
+import java.util.List;
 
 
 public class StatsActivity extends Activity {
@@ -23,16 +27,35 @@ public class StatsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Delays");
-        query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Delay");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> delays, ParseException e) {
                 if (e == null) {
-
+                    Log.d("score", "Retrieved " + delays.size() + " delays");
+                    drawStats(delays);
                 } else {
-                    Log.d(TAG, "Failed to get data from Parse");
+                    Log.d("score", "Error: " + e.getMessage());
                 }
             }
         });
+    }
+
+    public void drawStats(List delays) {
+
+        TextView delaysStatsTextView = (TextView) findViewById(R.id.delaysStatsTextView);
+
+        String output = "";
+        ParseObject delay;
+        int delayInMinutes = 0;
+
+        for (int i=0; i<delays.size(); i++) {
+            delay = (ParseObject) delays.get(i);
+            delayInMinutes = delay.getInt("delay");
+            output = output + Integer.toString(delayInMinutes) + "\n";
+        }
+
+        delaysStatsTextView.setText(output);
+
     }
 
 }
