4324a5b38d9cce90828ab92e56ede872195da541
Sebastian Björkelid <sebastian.bjorkelid@gmail.com>
2014-09-16 08:50:23 +0200

    Week stats working

--- app/src/main/java/com/bjorkelid/delays/StatsActivity.java
+++ app/src/main/java/com/bjorkelid/delays/StatsActivity.java
@@ -15,7 +15,16 @@ import com.parse.ParseAnalytics;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 
 public class StatsActivity extends Activity {
@@ -27,35 +36,160 @@ public class StatsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
+
+
+        fetchWeekStats();
+        fetchMonthStats();
+    }
+
+    public void fetchWeekStats() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        final int currentDay = calendar.get(Calendar.DAY_OF_YEAR); // 252
+
+        calendar.add(Calendar.DAY_OF_YEAR, -7);
+        Date oneWeekAgo = calendar.getTime();
+
+
+
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Delay");
+        query.whereGreaterThan("createdAt", oneWeekAgo);
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> delays, ParseException e) {
+
+                HashMap<String, Integer> groupedDelaysWeekMap = new HashMap<String, Integer>();
+
+                groupedDelaysWeekMap.put("Mon", 0);
+                groupedDelaysWeekMap.put("Tue", 0);
+                groupedDelaysWeekMap.put("Wed", 0);
+                groupedDelaysWeekMap.put("Thu", 0);
+                groupedDelaysWeekMap.put("Fri", 0);
+                groupedDelaysWeekMap.put("Sat", 0);
+                groupedDelaysWeekMap.put("Sun", 0);
+
                 if (e == null) {
                     Log.d("score", "Retrieved " + delays.size() + " delays");
-                    drawStats(delays);
+                    ParseObject delay;
+                    for (int i = 0; i < delays.size(); i++) {
+                        delay = (ParseObject) delays.get(i);
+                        calendar.setTime(delay.getCreatedAt());
+                        String dayLongName = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault());
+
+                        groupedDelaysWeekMap.put(dayLongName, groupedDelaysWeekMap.get(dayLongName) + delay.getInt("delay"));
+                    }
+
+
+                    Log.d(TAG, "KLAR");
+                    drawWeekStats(groupedDelaysWeekMap);
                 } else {
-                    Log.d("score", "Error: " + e.getMessage());
+                    Log.d(TAG, "Error: " + e.getMessage());
                 }
             }
         });
     }
 
-    public void drawStats(List delays) {
+    public void drawWeekStats(HashMap<String, Integer> groupedDelaysWeekMap) {
 
-        TextView delaysStatsTextView = (TextView) findViewById(R.id.delaysStatsTextView);
+        ValueLineChart weekChart = (ValueLineChart) findViewById(R.id.cubiclinechartWeek);
 
-        String output = "";
-        ParseObject delay;
-        int delayInMinutes = 0;
+        // Week
+        ValueLineSeries weekSeries = new ValueLineSeries();
+        weekSeries.setColor(0xccb7c0c7);
 
-        for (int i=0; i<delays.size(); i++) {
-            delay = (ParseObject) delays.get(i);
-            delayInMinutes = delay.getInt("delay");
-            output = output + Integer.toString(delayInMinutes) + "\n";
+
+        weekSeries.addPoint(new ValueLinePoint("", 0));
+
+        weekSeries.addPoint(new ValueLinePoint("Mon", groupedDelaysWeekMap.get("Mon")));
+        weekSeries.addPoint(new ValueLinePoint("Tue", groupedDelaysWeekMap.get("Tue")));
+        weekSeries.addPoint(new ValueLinePoint("Wed", groupedDelaysWeekMap.get("Wed")));
+        weekSeries.addPoint(new ValueLinePoint("Thu", groupedDelaysWeekMap.get("Thu")));
+        weekSeries.addPoint(new ValueLinePoint("Fri", groupedDelaysWeekMap.get("Fri")));
+        weekSeries.addPoint(new ValueLinePoint("Sat", groupedDelaysWeekMap.get("Sat")));
+        weekSeries.addPoint(new ValueLinePoint("Sun", groupedDelaysWeekMap.get("Sun")));
+
+        weekSeries.addPoint(new ValueLinePoint("", 0));
+
+
+        //delaysStatsTextView.setText(output);
+
+        weekChart.addSeries(weekSeries);
+        weekChart.startAnimation();
+
+
+    }
+
+    public void fetchMonthStats() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        final int currentDay = calendar.get(Calendar.DAY_OF_YEAR); // 252
+
+        calendar.add(Calendar.DAY_OF_YEAR, -30);
+        Date oneMonthAgo = calendar.getTime();
+
+
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Delay");
+        query.whereGreaterThan("createdAt", oneMonthAgo);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> delays, ParseException e) {
+
+                HashMap<String, Integer> groupedDelaysMonthMap = new HashMap<String, Integer>();
+
+                for(int i=0;i<30;i++) {
+                    groupedDelaysMonthMap.put(String.valueOf(i), 0);
+                }
+
+                if (e == null) {
+                    Log.d("score", "Retrieved " + delays.size() + " delays");
+                    ParseObject delay;
+                    for (int i = 0; i < delays.size(); i++) {
+                        delay = (ParseObject) delays.get(i);
+                        calendar.setTime(delay.getCreatedAt());
+                        int dayNumber = calendar.get(Calendar.DAY_OF_MONTH);
+
+                        groupedDelaysMonthMap.put(String.valueOf(dayNumber), groupedDelaysMonthMap.get(String.valueOf(dayNumber)) + delay.getInt("delay"));
+                    }
+
+
+                    Log.d(TAG, "KLAR");
+                    drawMonthStats(groupedDelaysMonthMap);
+                } else {
+                    Log.d(TAG, "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void drawMonthStats(HashMap<String, Integer> groupedDelaysMonthMap) {
+
+        ValueLineChart monthChart = (ValueLineChart) findViewById(R.id.cubiclinechartMonth);
+
+        // Month
+        ValueLineSeries monthSeries = new ValueLineSeries();
+        monthSeries.setColor(0xccb7c0c7);
+
+        for(String day : groupedDelaysMonthMap.keySet()) {
+            String value = String.valueOf(groupedDelaysMonthMap.get(day));
+            monthSeries.addPoint(new ValueLinePoint(value, groupedDelaysMonthMap.get(day)));
         }
 
-        delaysStatsTextView.setText(output);
+        monthChart.addSeries(monthSeries);
+        monthChart.startAnimation();
+
 
     }
 
 }
+
+ /*groupedDelaysMonthMap.put("Jan", 0);
+                groupedDelaysMonthMap.put("Feb", 0);
+                groupedDelaysMonthMap.put("Mar", 0);
+                groupedDelaysMonthMap.put("Apr", 0);
+                groupedDelaysMonthMap.put("May", 0);
+                groupedDelaysMonthMap.put("Jun", 0);
+                groupedDelaysMonthMap.put("Jul", 0);
+                groupedDelaysMonthMap.put("Aug", 0);
+                groupedDelaysMonthMap.put("Sep", 0);
+                groupedDelaysMonthMap.put("Oct", 0);
+                groupedDelaysMonthMap.put("Nov", 0);
+                groupedDelaysMonthMap.put("Dec", 0);*/
